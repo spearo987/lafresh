60d50f7d8011338335e210370715e55326960925
Athony <anthony.gerbenne@gmail.com>
2017-12-12 17:41:42 +0100

    Modif login form

--- views/login_form.php
+++ views/login_form.php
@@ -2,12 +2,12 @@
 
 <h2>Login</h2>
 
-<form class="signup-form" action="?action=login" method="post" name="login">
+<form class="signin-form" action="?action=login" method="post" name="login">
 	<label for="login">Login&nbsp;</label>
 	<input type="text" id="login" name="login" value="">
 	<label for="password">Password&nbsp;</label>
 	<input type="password" id="password" name="password" value="">
-	<a href="signup.php">Sign Up</a>
+	<a href="registers_form.php">Sign Up</a>
 	<input type="submit" value="Login">
 	<input type="checkbox" id="remember">
 	<label for="remember">Remember me</label>

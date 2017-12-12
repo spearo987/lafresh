05f426359dde2848bfa2780befb7d28eb1d943bc
Blust96 <quentin.bohain@gmail.com>
2017-12-12 16:36:55 +0100

    Delete routes.php

--- routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$routes = [	'home' => 'HomeController',
-			'login_form' => 'AuthController',
-			'login' => 'AuthController',
-			'register_form' => 'AuthController',
-			'register' => 'AuthController'];
